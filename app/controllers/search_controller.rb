0ec9c7d629fe66c78e3ffa93b71d630e4d5632a6
JunePaloma <kborkoski@gmail.com>
2017-12-13 10:28:16 -0700

    Add faraday connection object and response method to controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,13 @@
 class SearchController < ApplicationController
   def index
-  end 
+      @conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday_object|
+      faraday_object.adapter Faraday.default_adapter
+    end
+    @stations = search_by_state
+  end
+
+    def search_by_state
+    response = @conn.get("/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&radius=6&location=80203&limit=10&api_key=#{ENV["nrel_api_key"]}")
+    JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+    end
 end
